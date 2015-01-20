d5dad65877486eed7fc8d5691f6bb2e5b5f5499c
troyding <dingsword@gmail.com>
2015-01-20 15:29:43 +0800

    bug fix

--- storm-core/src/jvm/backtype/storm/serialization/KryoTupleSerializer.java
+++ storm-core/src/jvm/backtype/storm/serialization/KryoTupleSerializer.java
@@ -14,34 +14,33 @@ public class KryoTupleSerializer implements ITupleSerializer {
     Output _kryoOut;
 
     public KryoTupleSerializer(final Map conf,
-	    final GeneralTopologyContext context) {
-	_kryo = new KryoValuesSerializer(conf);
-	_kryoOut = new Output(2000, 2000000000);
-	_ids = new SerializationFactory.IdDictionary(context.getRawTopology());
+                               final GeneralTopologyContext context) {
+        _kryo = new KryoValuesSerializer(conf);
+        _kryoOut = new Output(2000, 2000000000);
+        _ids = new SerializationFactory.IdDictionary(context.getRawTopology());
     }
 
     public byte[] serialize(Tuple tuple) {
-	try {
+        try {
 
-	    _kryoOut.clear();
-	    _kryoOut.writeInt(tuple.getSourceTask(), true);
-	    _kryoOut.writeInt(
-		    _ids.getStreamId(tuple.getSourceComponent(),
-			    tuple.getSourceStreamId()), true);
-	    tuple.getMessageId().serialize(_kryoOut);
-	    _kryo.serializeInto(tuple.getValues(), _kryoOut);
-	    // serialize transfer start time to calc transfer cost
-	    Long transferStartTime = ((TupleImpl) tuple)
-		    .getExecuteSampleStartTime();
-	    if (transferStartTime == null) {
-		_kryoOut.writeLong(-1, true);
-	    } else {
-		_kryoOut.writeLong(transferStartTime, true);
-	    }
-	    return _kryoOut.toBytes();
-	} catch (IOException e) {
-	    throw new RuntimeException(e);
-	}
+            _kryoOut.clear();
+            _kryoOut.writeInt(tuple.getSourceTask(), true);
+            _kryoOut.writeInt(
+                    _ids.getStreamId(tuple.getSourceComponent(),
+                            tuple.getSourceStreamId()), true);
+            tuple.getMessageId().serialize(_kryoOut);
+            _kryo.serializeInto(tuple.getValues(), _kryoOut);
+            // serialize transfer start time to calc transfer cost
+            Long transferStartTime = ((TupleImpl) tuple).getTransferSampleStartTime();
+            if (transferStartTime == null) {
+                _kryoOut.writeLong(-1, true);
+            } else {
+                _kryoOut.writeLong(transferStartTime, true);
+            }
+            return _kryoOut.toBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     // public long crc32(Tuple tuple) {
     // try {
